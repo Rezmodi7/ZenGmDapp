5d4efed54599079e1216560bba252ca228d169f4
reza <rezamoaddab1998@gmail.com>
2025-09-21 14:24:26 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,240 @@
 // ABI & Contract Address
 const contractAddress = "0xf7a9Ac1aA90C914676dDC778cE85250745223F2E";
 const contractABI = [
-  { "inputs": [], "name": "clearRecords", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
-  { "inputs": [], "name": "gm", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
-  { "inputs": [{ "internalType": "uint256", "name": "_limit", "type": "uint256" }], "name": "setRecentLimit", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
-  { "inputs": [{ "internalType": "uint256", "name": "_offset", "type": "uint256" }], "name": "setTzOffsetSeconds", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
-  { "inputs": [], "stateMutability": "nonpayable", "type": "constructor" },
-  { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "user", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "timestamp", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "slotId", "type": "uint256" } ], "name": "GMSaid", "type": "event" },
-  { "inputs": [{ "internalType": "address", "name": "newOwner", "type": "address" }], "name": "transferOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
-  { "inputs": [], "name": "currentSlotId", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
-  { "inputs": [{ "internalType": "address", "name": "user", "type": "address" }], "name": "getGMCount", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
-  { "inputs": [{ "internalType": "address", "name": "user", "type": "address" }], "name": "getLastSlot", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
-  { "inputs": [{ "internalType": "uint256", "name": "count", "type": "uint256" }], "name": "getRecentGMs", "outputs": [{ "components": [ { "internalType": "address", "name": "user", "type": "address" }, { "internalType": "uint256", "name": "timestamp", "type": "uint256" }, { "internalType": "uint256", "name": "slotId", "type": "uint256" } ], "internalType": "struct GM.GMRecord[]", "name": "", "type": "tuple[]" }], "stateMutability": "view", "type": "function" },
-  { "inputs": [{ "internalType": "address", "name": "user", "type": "address" }], "name": "nextAllowedInfo", "outputs": [ { "internalType": "uint256", "name": "nextSlotId", "type": "uint256" }, { "internalType": "uint256", "name": "nextSlotStartTimestamp", "type": "uint256" } ], "stateMutability": "view", "type": "function" },
-  { "inputs": [], "name": "owner", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" },
-  { "inputs": [], "name": "recentLimit", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
-  { "inputs": [], "name": "tzOffsetSeconds", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }
-];
+{
+		"inputs": [],
+		"name": "clearRecords",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "gm",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_limit",
+				"type": "uint256"
+			}
+		],
+		"name": "setRecentLimit",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_offset",
+				"type": "uint256"
+			}
+		],
+		"name": "setTzOffsetSeconds",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "timestamp",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "slotId",
+				"type": "uint256"
+			}
+		],
+		"name": "GMSaid",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "currentSlotId",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			}
+		],
+		"name": "getGMCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			}
+		],
+		"name": "getLastSlot",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "count",
+				"type": "uint256"
+			}
+		],
+		"name": "getRecentGMs",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "address",
+						"name": "user",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "timestamp",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "slotId",
+						"type": "uint256"
+					}
+				],
+				"internalType": "struct GM.GMRecord[]",
+				"name": "",
+				"type": "tuple[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			}
+		],
+		"name": "nextAllowedInfo",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "nextSlotId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "nextSlotStartTimestamp",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "recentLimit",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "tzOffsetSeconds",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+] ;
 
 let provider, signer, contract;
 let transactions = [];
@@ -26,11 +244,11 @@ const connectBtn = document.getElementById("connectBtn");
 const userAddressEl = document.getElementById("userAddress");
 const gmBtn = document.getElementById("gmBtn");
 const gmCountEl = document.getElementById("gmCount");
-const recentList = document.getElementById("recentList");
 const viewTxBtn = document.getElementById("viewTxBtn");
 const txModal = document.getElementById("txModal");
 const closeModal = document.querySelector(".close");
 const txList = document.getElementById("txList");
+const faucetBtn = document.getElementById("faucetBtn");
 
 // Helper: short address
 function shortAddress(addr) {
@@ -72,28 +290,29 @@ gmBtn.onclick = async () => {
   }
 };
 
+// Faucet Button
+faucetBtn.onclick = () => {
+  window.open("https://faucet.zenchain.io", "_blank");
+};
+
 // Load Data
 async function loadData(address) {
   const count = await contract.getGMCount(address);
   gmCountEl.textContent = count.toString();
-
-  const recents = await contract.getRecentGMs(5);
-  recentList.innerHTML = "";
-  recents.forEach(r => {
-    const li = document.createElement("li");
-    li.textContent = `${shortAddress(r.user)} - ${formatTime(r.timestamp)}`;
-    recentList.appendChild(li);
-  });
 }
 
 // Modal Events
-viewTxBtn.onclick = () => {
+viewTxBtn.onclick = async () => {
   txList.innerHTML = "";
-  transactions.forEach(t => {
-    const p = document.createElement("p");
-    p.textContent = `${shortAddress(t.address)} - ${formatTime(t.timestamp)}`;
-    txList.appendChild(p);
-  });
+  // Load recent GMs dynamically
+  if (contract && signer) {
+    const recents = await contract.getRecentGMs(20);
+    recents.forEach(r => {
+      const p = document.createElement("p");
+      p.textContent = `${shortAddress(r.user)} - ${formatTime(r.timestamp)}`;
+      txList.appendChild(p);
+    });
+  }
   txModal.style.display = "block";
 };
 
